06fde4b0895a6016f8eed875c49a1b4ea6a0475e
김강산 <ksk6736@naver.com>
2020-07-06 11:16:02 +0900

    23

---
+++ Hello/src/main/java/com/bit/myapp/A.java
@@ -0,0 +1,17 @@
+package com.bit.myapp;
+
+public abstract class A {
+	protected String name;
+	
+	
+	
+	public int method1() {
+		return 0;
+	}
+	
+	protected abstract void me2();
+	private void me3() {
+		me2();
+	}
+	
+}
---
+++ Hello/src/main/java/com/bit/myapp/B.java
@@ -0,0 +1,11 @@
+package com.bit.myapp;
+
+public class B extends A{
+
+	@Override
+	protected void me2() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

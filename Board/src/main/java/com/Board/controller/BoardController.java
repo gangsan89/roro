c0977d5126a272b3a25fa5283165c58c88bd1704
gangsan89 <ksk6736@naver.com>
2020-08-28 02:15:08 +0900

    resources.static 아래 있는 폴더들은 전부 다운받은것 정리를 잘 해놓은것 같다 write.html 수정까지 했고 다음엔 BoardController에 게시글 등록을 처리하는 메소드 작성부터 이어서 하자

---
+++ Board/src/main/java/com/Board/controller/BoardController.java
@@ -0,0 +1,34 @@
+package com.Board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.Board.domain.BoardDTO;
+import com.Board.service.BoardService;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@GetMapping(value = "/board/write.do")
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) 
+								Long idx, Model model) {
+		if (idx == null) {
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			
+			if(board == null) {
+				return "redirect:/board/list.do";
+			}
+			model.addAttribute("board", board);
+		}
+				
+		return "board/write";
+	}
+}

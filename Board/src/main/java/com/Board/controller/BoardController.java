d581faf37a1e08e6a97272e8a1dd9f697e760cc8
gangsan89 <ksk6736@naver.com>
2020-08-31 21:23:36 +0900

    0831

--- Board/src/main/java/com/Board/controller/BoardController.java
+++ Board/src/main/java/com/Board/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.Board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Board.domain.BoardDTO;
@@ -30,5 +32,20 @@ public class BoardController {
 		}
 				
 		return "board/write";
+	}
+	//
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+		} catch (Exception e) {
+			// TODO: 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+		return "redirect:/board/list.do";
 	}
 }
